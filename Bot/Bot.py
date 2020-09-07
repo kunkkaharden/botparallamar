f504ce489201647701d80c1686809e432947bd4a
Adrian Gómez Reyes <adrigoreyes@gmail.com>
2020-09-08 00:56:23 +0200

    inicio del main

--- Bot/Bot.py
+++ Bot/Bot.py
@@ -1,27 +1,23 @@
 
 import json
-import Servicios
+#import Servicios
 import datetime
 import os
 import requests
 from flask import Flask, request
 from datetime import datetime,time
-from 
+from Utiles.BotUtiles import *
 
 BOT_URL = f'https://api.telegram.org/bot{os.environ["BOT_KEY"]}/'  # <-- add your telegram token as environment variable
 
 app = Flask(__name__)
 
-servicio = Servicios.Servicios()
 
 @app.route('/',methods=['Post'])
 def main():
     sms = request.json
     info = info_mensaje(sms)
-    print(sms)
-    if not info.id_persona == None:
-        if not info.is_bot:
-            servicio.analizarPersona(info.id_persona, info.persona)
+
     if not info.is_bot and info.tipo_sms == "texto":
         enviar_mensaje();
 
